79272635f1cbab0afeeaf807569db7b0b39a15fa
asolanaruiz <alberto.solana.ruiz@gmail.com>
2022-10-19 18:44:19 +0200

    Add main.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module form3-test
+
+go 1.19
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+  "github.com/asolanaruiz/form3-accountapi-client"
+)
+
+func main() {
+  client := f3accountapi.NewClient()
+}
