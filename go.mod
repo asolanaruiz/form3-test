16bb73c621af28a3c9c8a6b74f02e0876d3b6664
asolanaruiz <alberto.solana.ruiz@gmail.com>
2022-10-21 22:31:32 +0200

    create account

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module form3-test
 
 go 1.19
+
+replace github.com/asolanaruiz/form3-accountapi-client => ../form3-accountapi-client
+
+require (
+	github.com/asolanaruiz/form3-accountapi-client v0.0.0-00010101000000-000000000000 // indirect
+	github.com/google/uuid v1.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,9 +1,37 @@
 package main
 
 import (
-  "github.com/asolanaruiz/form3-accountapi-client"
+	"context"
+	"fmt"
+	"github.com/asolanaruiz/form3-accountapi-client"
+  "github.com/google/uuid"
 )
 
 func main() {
-  client := f3accountapi.NewClient()
+  client := form3.NewClient().WithUserAgent("asolanaruiz")
+	ctx := context.Background()
+
+	account, err := client.Create(createAccount(), ctx)
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("account: %v", *account)
+}
+
+
+func createAccount() form3.Account {
+  accountData := form3.AccountData{
+    Type: "accounts",
+    ID: uuid.New().String(),
+    OrganisationID: uuid.New().String(),
+    Attributes: form3.AccountAttributes{
+      Country: "GB",
+      BaseCurrency: "GBP",
+      BankID: "400300",
+	    BankIDCode: "ABDSC",
+      Bic: "NWBKGB22",
+      Name: []string{"fulanito"}}}
+  return form3.Account{Data: accountData}
 }
